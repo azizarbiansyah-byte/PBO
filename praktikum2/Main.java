eca9dcbf29622f69ec9412edb425e09a47030c41
azizarbiansyah-byte <azizarbiansyah6@gmail.com>
2025-10-25 12:31:08 +0700

    tugas 3 selesai dan kirim kembali tugas2

---
+++ praktikum2/Main.java
@@ -0,0 +1,26 @@
+package praktikum2;
+
+public class Main {
+    public static void main(String[] args) {
+        increment i1 = new increment();
+        i1.tampil();
+        
+        Segitiga s1 = new Segitiga();
+        s1.tampil();
+        
+        PythagorasKeliling p1 = new PythagorasKeliling();
+        p1.tampil();
+        
+        MengubahString m1 = new MengubahString();
+        m1.tampil();
+        
+        WaktuTempuh w1 = new WaktuTempuh();
+        w1.tampil();
+        
+        KonversiSuhu k1 = new KonversiSuhu();
+        k1.tampil();
+        
+        
+    }
+  
+}
